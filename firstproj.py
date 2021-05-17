64d54d5c908e8c3790ed59fedce914b7aa4e8b11
Vishal-Gajavelly <gajavelly2001@iitg.ac.in>
2021-05-17 17:28:39 +0530

    ff

--- firstproj.py
+++ firstproj.py
@@ -1,2 +1,4 @@
 print("New Project has started")
 print("Added new line")
+a = 5;
+print(a+5)
--- testchild.py
+++ testchild.py
@@ -1,3 +1,5 @@
 ## Adding child branch
 print("Name of the child branch")
 print("Added new line to child branch")
+
+
